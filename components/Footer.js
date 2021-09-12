2277eb7416c1f87e6110ea92133e17343c6358bc
David Andrews <dandrews@collain.com>
2021-09-11 21:56:33 -0500

    initial commit

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>NewsRoom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessability</p>
+        <p>This is not a real site</p>
+        <p>Its an awesome clone</p>
+        <p>Referals accepted</p>
+        <p>Join Now</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Papa React</p>
+        <p>Presents</p>
+        <p>Zero to Full Stack Hero</p>
+        <p>Hundreds of students</p>
+        <p>Join Now</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Center</p>
+        <p>Trust & Safety</p>
+        <p>Say Hi YouTube</p>
+        <p>Easter Eggs</p>
+        <p>For the win</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
