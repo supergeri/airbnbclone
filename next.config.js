c762e3aa5e1fb25ad57c0eaef658c67c96326f93
David Andrews <dandrews@collain.com>
2021-09-12 22:38:54 -0500

    added map

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ['links.papareact.com'],
   },
+  env: {
+    mapbox_key:
+      'pk.eyJ1Ijoic29vcGVyZ2VyaSIsImEiOiJja3RpMWtjdTcweGx2MnBvbHo5bW02dG8wIn0.KM5vpGd6JbV1V8sLtlw0aw',
+  },
 }
